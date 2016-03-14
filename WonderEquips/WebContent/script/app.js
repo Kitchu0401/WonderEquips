0f48eea78be0ac9db1389921d4e715e53c48c65a
Kitchu0401 <kitchu@naver.com>
2016-03-14 19:43:01 +0900

    merging resources.

--- WonderEquips/WebContent/script/app.js
+++ WonderEquips/WebContent/script/app.js
@@ -5,29 +5,66 @@
 angular.module('WonderEquips', ['ngCookies'])
 .controller('MainController', ['$scope', '$cookies', function($scope, $cookies) {
 	
+	$scope.watchedChampCount = 0;
+	
 	$scope.elementTag = ['불', '물', '나무', '빛', '어둠'];
 	$scope.typeTag = ['공격형', '방어형', '지원형'];
 	$scope.skillTag = ['무기', '방어구', '악세서리'];
 	
 	$scope.init = function() {
-		// retrieve user cookie data
-		$scope.weuserdata = $cookies.getObject('weuserdata');
-//		if ($scope.weuserdata) {
-//			alert($scope.weuserdata.name);
-//		}
+		$scope.champs = champs;
 		
-		$cookies.putObject('weuserdata', {name: 'kitchu'});
+		// retrieve user cookie data
+		var version = $cookies.get('we.version');
+		$scope.token = $cookies.get('we.user.token');
+		
+		var includeEmpty = $cookies.get('we.user.includeempty');
+		$scope.includeEmpty = includeEmpty ? includeEmpty : false;
+		
+		var watchids = $cookies.get('we.user.watchids');
+		var ids;
+		if (watchids) {
+			ids = watchids.split(',');
+			$scope.watchedChampCount = ids.length;
+			
+			for (var i = 0; i < ids.length; i++) {
+				for (var j = 0; j < $scope.champs.length; j++) {
+					if ($scope.champs[j].id == ids[i]) {
+						$scope.champs[j].watch = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		// if cached version is outdated, refresh cookie data
+		// and re-load all watchids to server
+		if (version != app.currentVersion) {
+			console.log('Update user version info and create new user token.');
+			
+			// update cached version
+			$cookies.put('we.version', app.currentVersion);
+			
+			// update cached user token
+			$scope.token = new Date().getTime();
+			$cookies.put('we.user.token', $scope.token);
+			
+			if (watchids) {
+				console.log('Request to load prewatched ids data to server.');
+				$scope.load(watchids);
+			}
+		}
 		
 		// reset all selector
 		$scope.reset();
 	}
 	
-	$scope.openChampPopup = function() {
-		console.log('pressed!');
-	}
-	
+	/**
+	 * functions in main page  
+	 */
 	$scope.selectPart = function(i) {
 		$scope.currentPart = i;
+		$scope.search();
 	}
 	
 	$scope.selectShape = function(i) {
@@ -56,10 +93,10 @@ angular.module('WonderEquips', ['ngCookies'])
 		}
 		
 		$scope.result = [];
-		for (idx in champs) {
-			var req = champs[idx].skill[$scope.currentPart];
-			if (req && $scope.check(req)) {
-				$scope.result.push(champs[idx]);
+		for (idx in $scope.champs) {
+			var req = $scope.champs[idx].skill[$scope.currentPart];
+			if (($scope.includeEmpty) || req && $scope.check(req)) {
+				$scope.result.push($scope.champs[idx]);
 			}
 		}
 	}
@@ -88,9 +125,63 @@ angular.module('WonderEquips', ['ngCookies'])
 		return unsolved <= 0;
 	}
 	
+	/**
+	 * functions in popup window
+	 */
+	$scope.toggleIncludeEmpty = function() {
+		$scope.includeEmpty = !$scope.includeEmpty;
+		$cookies.put('we.user.includeempty', $scope.includeEmpty);
+	}
+	
+	$scope.getClassIncludeEmpty = function() {
+		return $scope.includeEmpty ? "active" : "";
+	}
+	
+	$scope.searchFilter = function(champ) {
+		return !$scope.popupSearchText || $scope.popupSearchText.length <= 0
+			|| champ.name.indexOf($scope.popupSearchText) >= 0;
+	}
+	
+	$scope.toggleWatch = function(id) {
+		for (var i = 0; i < $scope.champs.length; i++) {
+			if ($scope.champs[i].id == id) {
+				$scope.champs[i].watch = !$scope.champs[i].watch;
+				if ($scope.champs[i].watch) {
+					$scope.load($scope.champs[i].id);
+				}
+				break;
+			}
+		}
+		
+		$cookies.put('we.user.watchids', $scope.getWatchedChampIds());
+	}
+	
+	// data load function
+	$scope.load = function(ids) {
+		$.ajax({
+			url: '/WonderEquips/service/load' 
+				+ '/' + app.currentVersion
+				+ '/' + $scope.token
+				+ '/' + ids,
+			method: 'POST'
+		});
+	}
+	
 	// utility functions
 	$scope.range = function(n) {
 		return new Array(n);
+	}
+	
+	$scope.getWatchedChampIds = function() {
+		var ids = [];
+		for (var i = 0; i < $scope.champs.length; i++) {
+			if ($scope.champs[i].watch) {
+				ids.push($scope.champs[i].id);
+			}
+		}
+		
+		$scope.watchedChampCount = ids.length;
+		return ids;
 	}
 	
 }]);
